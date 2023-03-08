bb67f242f2d031eb8677d15737bee6c41fc5a4c1
RUDYGAMES <rudygamesv2@gmail.com>
2023-03-08 21:31:34 +0100

    added site

---
+++ js/main.js
@@ -0,0 +1,70 @@
+const nav = document.querySelector('.nav');
+const navBtn = document.querySelector('.burger-btn');
+const allNavItems = document.querySelectorAll('.nav__item');
+const navBtnBars = document.querySelector('.burger-btn__bars');
+const allSections = document.querySelectorAll('.section');
+const footerYear = document.querySelector('.footer__year');
+
+
+const handleNav = () => {
+    nav.classList.toggle('nav--active');
+
+    allNavItems.forEach(item => {
+        item.addEventListener('click', () => {
+            nav.classList.remove('nav--active');
+        })
+    });
+
+    handleNavItemsAnimation();
+    deleteAnimation()
+}
+
+const handleNavItemsAnimation = () => {
+    let delayTime = 0;
+
+    allNavItems.forEach(item => {
+        item.classList.toggle('nav-items-animation');
+        item.style.animationDelay = `.${delayTime}s`;
+        delayTime++;
+    })
+}
+
+const deleteAnimation = () => {
+    allNavItems.forEach(item => {
+        item.addEventListener('click', () => {
+            allNavItems.forEach(el => {
+                el.classList.remove('nav-items-animation');
+            })
+        })
+    })
+}
+
+
+const handleCurrentYear = () => {
+    const year = (new Date).getFullYear();
+    footerYear.innerText = year;
+}
+
+const handleObserver = () => {
+    const currentSection = window.scrollY;
+
+    allSections.forEach(section => {
+
+        if (section.classList.contains('white-section') && section.offsetTop <= currentSection + 60) {
+
+            // sprawdzamy czy nasza sekcja jest w odpowiednim miejs (czuj juz tam sie zescrollowalismy)
+            // doajemy 60 bo nasze dodatki maja 50px
+            navBtnBars.classList.add('black-bars-color');
+        }
+        else if (!section.classList.contains('white-section') && section.offsetTop <= currentSection + 60) {
+            navBtnBars.classList.remove('black-bars-color');
+        }
+
+    })
+
+}
+
+
+handleCurrentYear();
+navBtn.addEventListener('click', handleNav);
+window.addEventListener('scroll', handleObserver)
